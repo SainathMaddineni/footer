377af29fa75c340a666319c699d351f46ef199c4
ThirupathiRaju52 <thiru@sloperateinc.com>
2020-10-21 06:40:12 -0400

    enhanced code

---
+++ src/components/footer.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+    state = {  }
+    render() {
+        return (
+            <div>
+                <footer>
+                      <p style={{textAlign: 'center'}}>Copyright © 2020 Slope Rate Tech Private Limited - All Rights Reserved. </p>
+                </footer>
+            </div>
+         );
+    }
+}
+
+export default Footer;
---
+++ src/components/services.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import {Link,BrowserRouter as Router} from 'react-router-dom';
+class Services extends Component {
+    state = {  }
+    render() {
+        return (
+            <div>
+                <h1 style={{textAlign: 'center'}}>System under maintenance</h1></div>
+         );
+    }
+}
+
+export default Services;
