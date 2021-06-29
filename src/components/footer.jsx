278fcf49b4735998af11375bcfed6ade26c39528
gireeshsloperate <gireeshkumar@sloperateinc.com>
2021-06-29 22:00:12 +0530

    footerchanged

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -5,6 +5,15 @@ class Footer extends Component {
   render() {
     return (
       <div>
+        <section id = "newsletter">
+            <div className ="container1" id = "container1input">
+              <h1> Subscribe to our news update</h1>
+              <form>
+                <input type ="email" placeholder="Enter email"/>
+                <button type="submit" className ="button_1">Subscribe</button>
+                </form>
+                </div>
+            </section>
         <footer>
           <p style={{ textAlign: "center" }}>
             Copyright © 2020 Slope Rate Techno Private Limited - All Rights
