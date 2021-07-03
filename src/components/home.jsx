54940aedd0935bd795152c1547493b56aa1ebd88
Lavanyasloperate <lavanyadurga.dondapati@sloperateinc.com>
2021-07-03 15:57:11 +0530

    changes in footer is done

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -16,7 +16,7 @@ class Home extends Component {
           <p  id = "para">Software innovation,Like almost every other kind of
             innovation,requires the ability to collaborate and share ideas with other people,
             and to sit down and talk with customers and get their feedback and understand their
-            needs....</p>
+            needs.....</p>
         </div>
           </section>
             <section id="boxes">
